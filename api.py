c578cb15932da8a31014083250518a4d15a7709d
Andrew Stubbs <andrew@kidsplay.info>
2013-10-01 16:51:55 +0100

    Implement basic location queries.
    Database queries cannot do range checks on both longitude and latitude
    at once, so we use a simple geohash to do both in one. Basically, the
    coordinates are divided into buckets, and we search for everything in that
    bucket at once.
    
    Doing proper geohashing that works evenly across the whole planet seems
    pointless given that there won't be many locations near the poles.

--- api.py
+++ api.py
@@ -1,18 +1,49 @@
 import webapp2
 from webapp2_extras import json
 from google.appengine.ext import ndb
+import logging
 
 class Location(ndb.Model):
     loc = ndb.GeoPtProperty()
+    geohash = ndb.StringProperty()
     name = ndb.StringProperty()
     address = ndb.StringProperty(repeated=True)
     notes = ndb.TextProperty()
+
+    L1_hashsize = 0.05
+
+    @classmethod
+    def make_geohash_L1(cls, lon, lat):
+	# round to nearest 0.05
+	x = round(float(lat)*20, 0)/20
+	y = round(float(lon)*20, 0)/20
+	if x == -0.0:
+	    x = 0.0
+	if y == -0.0:
+	    y = 0.0
+	return str(x) + ":" + str(y)
+
+    @classmethod
+    def query_bbox(cls, north, east, south, west):
+	squares = []
+	x = west
+	while x >= east:
+	    y = north
+	    while y >= south:
+		squares.append(cls.make_geohash_L1(x, y))
+		y -= cls.L1_hashsize
+	    x -= cls.L1_hashsize
+	if len(squares) > 0:
+	    return cls.query(Location.geohash.IN(squares))
+	else:
+	    return []
 
 class LocationAPI(webapp2.RequestHandler):
 
     def post(self, arg):
 	loc = Location()
 	loc.loc = ndb.GeoPt(float(self.request.get('latitude')), float(self.request.get('longitude')))
+	loc.geohash = Location.make_geohash_L1(loc.loc.lat, loc.loc.lon)
 	loc.name = self.request.get('name')
 	loc.address = [self.request.get('address1'),
 		       self.request.get('address2'),
@@ -25,19 +56,30 @@ class LocationAPI(webapp2.RequestHandler):
     def get(self,id):
 	if id == '':
 	    # search requires parameters
-	    self.response.write("not implemented")
+	    west = float(self.request.get('west'))
+	    east = float(self.request.get('east'))
+	    north = float(self.request.get('north'))
+	    south = float(self.request.get('south'))
+	    data = []
+	    for loc in Location.query_bbox(north, east, south, west):
+		data.append({"lon": loc.loc.lon,
+			     "lat": loc.loc.lat,
+			     "name": loc.name,
+			     "address": loc.address,
+			     "notes": loc.notes})
+
 	else:
 	    # client requested a specific location
 	    key = ndb.Key(urlsafe=id)
 	    loc = key.get()
 	    data = {"id": id,
-		    "long": loc.loc.lon,
+		    "lon": loc.loc.lon,
 		    "lat": loc.loc.lat,
 		    "name": loc.name,
 		    "address": loc.address,
 		    "notes": loc.notes}
-	    self.response.headers['Content-Type'] = 'application/json'
-	    self.response.write(json.encode(data))
+	self.response.headers['Content-Type'] = 'application/json'
+	self.response.write(json.encode(data))
 
 api = webapp2.WSGIApplication([
 	 ('/api/v1.0/locations/(.*)', LocationAPI)
